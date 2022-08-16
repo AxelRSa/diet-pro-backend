213fa59ccbe960cfe655c4d3851e34e1cff8b8ff
AxelRSa <axelg.rizos@gmail.com>
2022-08-16 12:31:23 -0500

    feat: fix validation format to better understand
    modified:   app/modules/auth/auth.validation.js - fix validation format
    modified:   app/modules/food/food.validation.js - fix validation format
    modified:   app/modules/person/person.validation.js - fix validation format

--- app/modules/auth/auth.validation.js
+++ app/modules/auth/auth.validation.js
@@ -2,37 +2,16 @@ const { check } = require("express-validator")
 const { validateResult } = require("../../helpers/validateHelper")
 
 const login = [
-  check("email")
-    .exists()
-    .notEmpty()
-    .isEmail(),
-
-  check("password")
-    .exists()
-    .notEmpty(),
-
-  (req, res, next) => {
-    validateResult(req, res, next)
-  }
+  check("email").exists().notEmpty().isEmail(),
+  check("password").exists().notEmpty(),
+  (req, res, next) => validateResult(req, res, next)
 ]
 
 const signup = [
-  check("email")
-    .exists()
-    .notEmpty()
-    .isEmail(),
-
-  check("password")
-    .exists()
-    .notEmpty(),
-
-  check("username")
-    .exists()
-    .notEmpty(),
-
-  (req, res, next) => {
-    validateResult(req, res, next)
-  }
+  check("email").exists().notEmpty().isEmail(),
+  check("password").exists().notEmpty(),
+  check("username").exists().notEmpty(),
+  (req, res, next) => validateResult(req, res, next)
 ]
 
 module.exports = {
--- app/modules/food/food.validation.js
+++ app/modules/food/food.validation.js
@@ -2,33 +2,12 @@ const { check } = require("express-validator")
 const { validateResult } = require("../../helpers/validateHelper")
 
 const createFood = [
-  check("idUser")
-    .exists()
-    .notEmpty(),
-
-  check("name")
-    .exists()
-    .notEmpty()
-    .isString(),
-
-  check("protein")
-    .exists()
-    .notEmpty()
-    .isNumeric(),
-
-  check("carbohydrates")
-    .exists()
-    .notEmpty()
-    .isNumeric(),
-
-  check("fat")
-    .exists()
-    .notEmpty()
-    .isNumeric(),
-
-  (req, res, next) => {
-    validateResult(req, res, next)
-  }
+  check("idUser").exists().notEmpty(),
+  check("name").exists().notEmpty().isString(),
+  check("protein").exists().notEmpty().isNumeric(),
+  check("carbohydrates").exists().notEmpty().isNumeric(),
+  check("fat").exists().notEmpty().isNumeric(),
+  (req, res, next) => validateResult(req, res, next)
 ]
 
 module.exports = {
