d95ef5091b0e88d3dec899ad525b3c1d60a8ca53
AxelRSa <axelg.rizos@gmail.com>
2022-08-23 13:00:28 -0500

    feat: order results in food gets
    modified:   app/services/food.service.js - add an "ORDER BY ASC"

--- app/services/food.service.js
+++ app/services/food.service.js
@@ -36,7 +36,7 @@ const getFoodsByIdUserWithLimits = async (idUser, limitStart, limitEnd) => {
       FROM foods as f
       LEFT JOIN foods_measures as fm ON f.id_food = fm.id_food
       WHERE id_user = ?
-      ORDER BY f.name DESC
+      ORDER BY f.name ASC
       LIMIT ?, ?
       `
     return await query(sql, [idUser, limitStart, limitEnd])
@@ -88,6 +88,7 @@ const getFoodByIdUserAndIdFood = async (idUser, idFood) => {
       SELECT f.id_food as idFood, f.name, f.carbohydrates, f.protein, f.fat, fm.measure_name as measureName, fm.quantity, fm.id_food_measure as idMeasure
       FROM foods as f
       LEFT JOIN foods_measures as fm ON f.id_food = fm.id_food
+      ORDER BY f.name ASC
       WHERE id_user = ?
       AND f.id_food = ?
       `
