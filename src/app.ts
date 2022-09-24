e24faec4cd206f14cd992bcf4613de08caeb357f
AxelRSa <axelg.rizos@gmail.com>
2022-09-24 11:20:40 -0500

    docs(app): add new comments and change format

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,39 @@
-// libraries
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
-// routes
+/* Importing the libraries. */
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+/* Importing the routes */
 import authRouter from './app/modules/auth/auth.router'
 
-// app
-const app: Express = express();
+/* Creating an instance of express. */
+const app: Express = express()
 
-// environment variables
+/* Setting the port to 5000 if the process.env.PORT is not set. */
 const port: number = Number(process.env.PORT) || 5000
 
-// env variables
-// https://www.npmjs.com/package/dotenv
-dotenv.config();
+/* https://www.npmjs.com/package/dotenv */
+/* Loading the environment variables from the .env file. */
+dotenv.config()
 
-// middleware
-// body parser for json info
+/* Middleware */
+/* A middleware that parses the body of the request and makes it available in the req.body property. */
 app.use(express.json())
-// cors usage https://www.npmjs.com/package/cors
+/* https://www.npmjs.com/package/cors */
+/* Allowing the server to accept requests from other domains. */
 app.use(cors())
-// morgan https://www.npmjs.com/package/morgan
+/* morgan https://www.npmjs.com/package/morgan */
+/* A middleware that logs the requests to the console. */
 app.use(morgan('tiny'))
 
-// use routes
-app.use("/api/auth", authRouter)
+/* Use routes */
+app.use('/api/auth', authRouter)
 // app.use("/api/users", require("./app/modules/users/users.route"))
 // app.use("/api/persons", require("./app/modules/persons/persons.route"))
 // app.use("/api/foods", require("./app/modules/foods/foods.route"))
 // app.use("/api/meals", require("./app/modules/meals/meals.route"))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-// server initialization
+/* Starting the server. */
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+  console.log(`Server is running on http://localhost:${port}/`)
 })
