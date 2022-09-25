302578ec96462d9ad009cdce06956c4269f9a491
AxelRSa <axelg.rizos@gmail.com>
2022-09-25 12:26:22 -0500

    docs(auth): signup function description

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -2,13 +2,21 @@ import { Request, Response } from 'express'
 import { encrypt, compare } from '../../helpers/handleBcrypt'
 import * as usersService from '../../services/users.service'
 
+/**
+ * signup controller
+ * @param {Request} req - Request - The request object
+ * @param {Response} res - Response - The response object that will be sent back to the client.
+ * @returns The response is being returned.
+ */
 export const signup = async (req: Request, res: Response) => {
   try {
     const { email, password, username } = req.body as { email: string, password: string, username: string }
 
+    /* Checking if the email exist in the database. */
     const emailsResponse = await usersService.getEmailByEmail(email) 
     if (emailsResponse.length > 0) throw new Error('The email exist, try with another')
 
+    /* Encrypting the password and then creating the user. */
     const hashedPassword = await encrypt(password)
     await usersService.createUser(email, hashedPassword, username)
 
