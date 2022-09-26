afa798ee0342ddb79072f440b7fdf8f49a6926ed
AxelRSa <axelg.rizos@gmail.com>
2022-09-26 16:08:30 -0500

    feat(error): use new middleware in controllers

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -1,5 +1,6 @@
+import { CustomError } from './../../helpers/generateErrors'
 import { tokenSign, verifyToken } from './../../helpers/generateToken'
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { encrypt, compare } from '../../helpers/handleBcrypt'
 import * as usersService from '../../services/users.service'
 
@@ -9,29 +10,23 @@ import * as usersService from '../../services/users.service'
  * @param res - Response - The response object that will be sent back to the client.
  * @returns The response is being returned.
  */
-export const signup = async (req: Request, res: Response) => {
-  try {
-    const { email, password, username } = req.body as { email: string, password: string, username: string }
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password, username } = req.body as { email: string, password: string, username: string }
 
-    /* Checking if the email exist in the database. */
-    const emailsResponse = await usersService.getEmailByEmail(email)
-    if (emailsResponse.length > 0) throw new Error('The email exist, try with another')
+  /* Checking if the email exist in the database. */
+  const emailsResponse = await usersService.getEmailByEmail(email)
+  if (emailsResponse.length > 0)  return next(new CustomError(400, 'The email exist, try with another'))
 
-    /* Encrypting the password and then creating the user. */
-    const hashedPassword = await encrypt(password)
-    await usersService.createUser(email, hashedPassword, username)
+  /* Encrypting the password and then creating the user. */
+  const hashedPassword = await encrypt(password)
+  await usersService.createUser(email, hashedPassword, username)
 
-    const response = {
-      status: 'success',
-      message: 'The user was created successfully'
-    }
-
-    return res.json(response)
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({ status: 'error', message: error.message })
-    }
+  const response = {
+    status: 'success',
+    message: 'The user was created successfully'
   }
+
+  return res.json(response)
 }
 
 /**
@@ -39,35 +34,30 @@ export const signup = async (req: Request, res: Response) => {
  * compares the password that the user sent with the password that is stored in the database, signs and
  * gets the token, and finally returns the token to the user.
  * @param req - Request - The request object
- * @param res - Response =&gt; The response object that will be sent to the client
+ * @param res - Response - The response object that will be sent to the client
  * @returns The token
  */
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body as { email: string, password: string }
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body as { email: string, password: string }
 
-    /* Checking if the email exist in the database and save the data in a constant */
-    const userData = await usersService.getUserByEmail(email)
-    if (userData.length === 0) throw new Error('The email is not registered, create an account')
-    const user = userData[0]
+  /* Checking if the email exist in the database and save the data in a constant */
+  const userData = await usersService.getUserByEmail(email)
+  if (userData.length === 0) return next(new CustomError(400, 'The email is not registered, create an account'))
+  const user = userData[0]
 
-    /* Comparing the password that the user sent with the password that is stored in the database, sign and get the token */
-    const passwordMatch = await compare(password, user.passwordUser)
-    if (!passwordMatch) throw new Error('The password is incorrect, verify it')
-    const token = await tokenSign(user)
+  /* Comparing the password that the user sent with the password that is stored in the database, 
+  sign and get the token to send to the user */
+  const passwordMatch = await compare(password, user.passwordUser)
+  if (!passwordMatch) return next(new CustomError(400, 'The password is incorrect, verify it'))
+  const token = await tokenSign(user)
 
-    const response = {
-      status: 'success',
-      message: `Welcome ${user.usernameUser}`,
-      data: { token }
-    }
-
-    res.json(response)
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({ status: 'error', message: error.message })
-    }
+  const response = {
+    status: 'success',
+    message: `Welcome ${user.usernameUser}`,
+    data: { token }
   }
+
+  return res.json(response)
 }
 
 /**
@@ -76,32 +66,25 @@ export const login = async (req: Request, res: Response) => {
  * @param res - Response - The response object that is returned by the express server.
  * @returns The user info email, id and username
  */
-export const getUser = async (req: Request, res: Response) => {
-  try {
-    const token = req.headers.authorization?.split(' ').pop()
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ').pop()
 
-    /* Checking if the token is undefined, if it is, it throws an error. */
-    if (token === undefined) { throw new Error('The auth token was not send') }
+  /* Checking if the token is undefined, if it is, it throws an error. */
+  if (token === undefined) { return next(new CustomError(400, 'The auth token was not send')) }
 
-    /* Verifying the token that the user sent, if the token is valid, it returns the data of the token that
+  /* Verifying the token that the user sent, if the token is valid, it returns the data of the token that
     was signed, if the token is not valid, it returns and Error. */
-    const decodedToken = await verifyToken(token)
-    if (decodedToken === null) { throw new Error('There is an problem with authentication, try again') }
-    const { id } = decodedToken
+  const decodedToken = await verifyToken(token)
+  if (decodedToken === null) { return next(new CustomError(401, 'There is an problem with authentication, try again')) }
+  const { id } = decodedToken
 
-    /* Destructuring the object that is returned by the function getUserById, and it is saving the values */
-    const [{ usernameUser: username, emailUser: email }] = await usersService.getUserById(id)
+  /* Destructuring the object that is returned by the function getUserById, and it is saving the values */
+  const [{ usernameUser: username, emailUser: email }] = await usersService.getUserById(id)
 
-    const response = {
-      status: 'success',
-      data: { user: { email, id, username } }
-    }
-
-    return res.json(response)
-
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({ status: 'error', message: error.message })
-    }
+  const response = {
+    status: 'success',
+    data: { user: { email, id, username } }
   }
+
+  return res.json(response)
 }
--- src/app/modules/users/users.controllers.ts
+++ src/app/modules/users/users.controllers.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import { CustomError } from './../../helpers/generateErrors'
 import * as personsService from '../../services/persons.service'
 // import * as foodsService from '../../services/foods.service'
 // import * as mealsService from '../../services/meals.service'
@@ -6,28 +7,18 @@ import * as personsService from '../../services/persons.service'
 /* Create */
 /**
  * It creates a person
- * @param {Request} req - Request - The request object.
- * @param {Response} res - Response - The response object.
+ * @param req - Request - The request object.
+ * @param res - Response - The response object.
  * @returns returns a response object
  */
-export const createPerson = async (req: Request, res: Response) => {
-  try {
-    const { idUser:idUserString } = req.params as {idUser:string}
-    const { name } = req.body as {name:string}
+export const createPerson = async (req: Request, res: Response, next:NextFunction) => {
+  const { idUser } = req.params as {idUser:string}
+  const { name } = req.body as {name:string}
 
-    /* Converting the string to a number. */
-    const idUser = +idUserString
+  /* Checking if the person exists and if not, create one. */
+  const personWithThatName = await personsService.getPersonByIdUserAndName(idUser, name)
+  if (personWithThatName.length >= 1) { return next(new CustomError(400, 'That name exists, please, choose another one')) }
+  await personsService.createPerson(idUser, name)
 
-    /* Checking if the person exists and if not, create one. */
-    const personWithThatName = await personsService.getPersonByIdUserAndName(idUser, name)
-    if (personWithThatName.length >= 1) throw new Error('That name exists, please, choose another one')
-    await personsService.createPerson(idUser, name)
-
-    return res.json({ status: 'success', message: `The person ${name} was created` })
-
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({ status: 'error', message: error.message })
-    }
-  }
+  return res.json({ status: 'success', message: `The person ${name} was created` })
 }
