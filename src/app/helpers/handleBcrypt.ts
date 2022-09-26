02c7ec2c90d469ee8e37679fbcb5c3cfa8aba548
AxelRSa <axelg.rizos@gmail.com>
2022-09-25 21:07:36 -0500

    docs: change jsdocs for typedocs

--- src/app/helpers/handleBcrypt.ts
+++ src/app/helpers/handleBcrypt.ts
@@ -1,25 +1,21 @@
 import * as bcrypt from 'bcryptjs'
 
 /**
- * @module handleBcrypt
- */
-
-/**
  * This function takes a string and returns a promise that resolves to a string.
- * @param {string} plainText - The text you want to encrypt.
- * @returns {Promise<string>} Give the password hashed
+ * @param plainText - The text you want to encrypt.
+ * @returns Give the password hashed
  */
-export const encrypt = async (plainText: string): Promise<string> => {
+export const encrypt = async (plainText: string) => {
   return await bcrypt.hash(plainText, 10)
 }
 
 /**
  * This function takes a plain text password and a hashed password and returns a promise that resolves
  * to a boolean value.
- * @param {string} passwordPlain - The password that the user entered
- * @param {string} hashPassword - The hashed password that you want to compare against.
- * @returns {Promise<boolean>} Give a boolean to know if the strings are equal
+ * @param passwordPlain - The password that the user entered
+ * @param hashPassword - The hashed password that you want to compare against.
+ * @returns Give a boolean to know if the strings are equal
  */
-export const compare = async (passwordPlain: string, hashPassword: string) : Promise<boolean>=> {
+export const compare = async (passwordPlain: string, hashPassword: string) => {
   return await bcrypt.compare(passwordPlain, hashPassword)
 }
