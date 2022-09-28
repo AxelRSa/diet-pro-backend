bfd06a83af3f4bf81e2b4795373c09f610d4345c
AxelRSa <axelg.rizos@gmail.com>
2022-09-27 20:19:39 -0500

    feat(foods): do POST "user/idUser/foods" endpoint

--- src/app/services/foods.service.ts
+++ src/app/services/foods.service.ts
@@ -1,21 +1,34 @@
 import pool from '../../config/db'
 import { makeAQueryToDataBase } from '../helpers/makeAQueryToDataBase'
-import { ResultSetHeader } from 'mysql2'
+import { ResultSetHeader, RowDataPacket } from 'mysql2'
 
 /* Create */
-export const createFood = async (idUser: string, name: string, protein: string, carbohydrates: string, fat: string) => {
+export const createFood = async (idUser: string, nameFood: string, proteinFood: string, carbohydratesFood: string, fatFood: string) => {
   const dataFromDB = await makeAQueryToDataBase<ResultSetHeader>(async () => {
     const sql =
       `
       INSERT INTO foods (id_user, name, protein, carbohydrates, fat)
       VALUES (?, ?, ?, ?, ?)
 			`
-    return await pool.query(sql, [idUser, name, protein, carbohydrates, fat])
+    return await pool.query(sql, [idUser, nameFood, proteinFood, carbohydratesFood, fatFood])
   })
   return dataFromDB
 }
 
 /* Read */
+export const getFoodsByIdUserAndName = async (idUser:string, nameFood:string) => {
+  const dataFromDB = await makeAQueryToDataBase<RowDataPacket[]>(async () => {
+    const sql =
+      `
+      SELECT name as nameFood
+      FROM foods
+      WHERE id_user = ?
+      AND name = ?
+			`
+    return await pool.query(sql, [idUser, nameFood])
+  })
+  return dataFromDB[0] as {nameFood:string}[]
+}
 
 /* Update */
 
