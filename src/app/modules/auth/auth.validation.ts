5fe3c641af6519c044ef24cce3da2fb80f9b30ff
AxelRSa <axelg.rizos@gmail.com>
2022-09-27 23:41:13 -0500

    docs(validation): put the method in the docs

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,14 +1,14 @@
 import { check } from 'express-validator'
 import { validateResult } from './../../helpers/validateHelper'
 
-/** Validation for "auth/login" route */
+/** Validation for POST "auth/login" route */
 export const login = [
   check('email').exists().notEmpty().isEmail(),
   check('password').exists().isString().notEmpty(),
   validateResult
 ]
 
-/** Validation for "auth/signup" route */
+/** Validation for POST "auth/signup" route */
 export const signup = [
   check('email').exists().notEmpty().isEmail(),
   check('password').exists().isString().notEmpty(),
